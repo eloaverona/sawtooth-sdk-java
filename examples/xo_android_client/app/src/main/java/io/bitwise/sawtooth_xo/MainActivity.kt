a54f330b7b8414f71b04431f64c706e2e9b3dd67
Eloá França Verona <eloafran@bitwise.io>
2018-11-14 10:11:48 -0600

    WIP

--- examples/xo_android_client/app/src/main/java/io/bitwise/sawtooth_xo/MainActivity.kt
+++ examples/xo_android_client/app/src/main/java/io/bitwise/sawtooth_xo/MainActivity.kt
@@ -2,11 +2,22 @@ package io.bitwise.sawtooth_xo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Create New Game button callback */
+    fun createNewGame(view: View) {
+        // Do something in response to button
+    }
+
+    /** List Games button callback */
+    fun listGames(view: View) {
+        // Do something in response to button
     }
 }
